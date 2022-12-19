de8d9ca91a1900fe105574e56e029c3e4e005ba8
anch2501 <117604461+anch2501@users.noreply.github.com>
2022-12-18 23:34:59 -0500

    Add files via upload

---
+++ PYTHON-SECUENCIALES-Y-CONDICIONALES/SECUENCIALES/15.PY
@@ -0,0 +1,6 @@
+num1 = float(input("Introduce el número 1:"))
+num2 = float(input("Introduce el número 2:"))
+print("La suma es", num1+num2)
+print("La resta es", num1-num2)
+print("La multiplicación es", num1*num2)
+print("La división es", num1/num2)
